74cc94340d62aa55427e903d2f6cd0e59a613707
victor-sha <kassini_90@mail.ru>
2019-10-01 20:01:09 +0300

    add login and logout routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { router } from './routes/loginRouters';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['session'] }));
 app.use(router);
 
 app.listen(3000, () => {
--- src/routes/loginRouters.ts
+++ src/routes/loginRouters.ts
@@ -1,11 +1,15 @@
 import { Router, Request, Response } from 'express';
+import { runInNewContext } from 'vm';
+
+interface RequestWithBody extends Request {
+    body: { [key: string]: string | undefined };
+}
 
 const router = Router();
 
-
-router.get('/', (req, res) => {
-    res.send("Hello there");
-});
+// router.get('/', (req, res) => {
+//     res.send('Hello there');
+// });
 
 router.get('/login', (req: Request, res: Response) => {
     res.send(`
@@ -21,11 +25,40 @@ router.get('/login', (req: Request, res: Response) => {
             <button type="submit">Login</div>
         </form>
     `);
-})
-
-router.post('/login', (req: Request, res: Response) => {
-    const {email, password} = req.body;
-    res.send(email + password);
 });
 
-export {router}
+router.post('/login', (req: RequestWithBody, res: Response) => {
+    const { email, password } = req.body;
+
+    if (email && password && email === 'hi@hi.com' && password === 'password') {
+        req.session = { loggedIn: true }
+        res.redirect('/');
+    } else {
+        res.send('Invalid email or password');
+    }
+});
+
+router.get('/', (req: Request, res: Response) => {
+    if (req.session && req.session.loggedIn) {
+        res.send(`
+            <div>
+                <div>You ar logged in</div>
+                <a href="/logout">Logout</a>
+            </div>
+        `)
+    } else {
+        res.send(`
+            <div>
+                <div>You are not logged in</div>
+                <a href="/logout">Logout</a>
+            </div>
+        `)
+    }
+})
+
+router.get('/logout', (req: Request, res: Response) => {
+    req.session = undefined;
+    res.redirect('/');
+})
+
+export { router };
