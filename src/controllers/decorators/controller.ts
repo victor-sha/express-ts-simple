212507977239e6579ab5b6b739c80e193583e56b
victor-sha <kassini_90@mail.ru>
2019-10-11 15:43:41 +0300

    add difficalt controller

---
+++ src/controllers/decorators/controller.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import { AppRouter } from '../../AppRouter';
+
+const router = AppRouter.getInstance();
+
+export function controller(routePrefix: string) {
+  return function (target: Function) {
+    for (let key in target.prototype) {
+      const routeHandler = target.prototype[key];
+      const path = Reflect.getMetadata('path', target.prototype, key);
+
+      if (path) {
+        router.get(`${routePrefix}${path}`, routeHandler);
+      }
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { router } from './routes/loginRouters';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import { AppRouter } from './AppRouter'
+import './controllers/LoginController';
 
 const app = express();
+const router = AppRouter.getInstance();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['session'] }));
