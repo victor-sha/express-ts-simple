d4122256e666d4761c8adc9ffe6c77fbb324f42d
victor-sha <kassini_90@mail.ru>
2019-10-15 19:41:37 +0300

    add routerBinder - creator of http request decorators refactor string literals with enums

---
+++ src/controllers/decorators/Methods.ts
@@ -0,0 +1,7 @@
+export enum Methods {
+    get = 'get',
+    put = 'put',
+    post = 'post',
+    del = 'delete',
+    patch = 'patch'
+}
--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
+import {Methods} from './Methods';
+import {MetadataKeys} from './MetadataKeys';
 
 
 export function controller(routePrefix: string) {
@@ -8,10 +10,11 @@ export function controller(routePrefix: string) {
 
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
-      const path = Reflect.getMetadata('path', target.prototype, key);
+      const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key);
+      const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key);
 
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, routeHandler);
       }
     }
   }
