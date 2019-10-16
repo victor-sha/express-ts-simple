933cf2ecd382180454637907646f391d0a8e1e96
victor-sha <kassini_90@mail.ru>
2019-10-16 11:08:23 +0300

    add use middleware decorator

--- src/routes/loginRouters.ts
+++ src/routes/loginRouters.ts
@@ -11,21 +11,21 @@ const router = Router();
 //     res.send('Hello there');
 // });
 
-router.get('/login', (req: Request, res: Response) => {
-    res.send(`
-        <form method="POST">
-            <div>
-                <label>Email: </label>
-                <input name="email" type="email" />
-            </div>
-            <div>
-                <label>Password: </label>
-                <input name="password" type="password" />
-            </div>
-            <button type="submit">Login</div>
-        </form>
-    `);
-});
+// router.get('/login', (req: Request, res: Response) => {
+//     res.send(`
+//         <form method="POST">
+//             <div>
+//                 <label>Email: </label>
+//                 <input name="email" type="email" />
+//             </div>
+//             <div>
+//                 <label>Password: </label>
+//                 <input name="password" type="password" />
+//             </div>
+//             <button type="submit">Login</div>
+//         </form>
+//     `);
+// });
 
 router.post('/login', (req: RequestWithBody, res: Response) => {
     const { email, password } = req.body;
